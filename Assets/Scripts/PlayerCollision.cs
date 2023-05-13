7e028501d0bc5155b6e1fe6bb4cf9f12d419b38a
EPI2023 <kosma.kierek@student.uj.edu.pl>
2023-05-13 19:11:59 +0200

    fff

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    //reference to PlayerMovement script
+
+    //this function runs when we hit another object
+    //we get info about the collision and call it "collisionInfo"
+   void OnCollisionEnter (Collision collisionInfo)
+    {
+        //Debug.Log(collisionInfo.collider.name);
+
+        //we check if the collider has the "Obstacle" tag
+        if (collisionInfo.collider.tag=="Obstacle")
+        {
+            //Debug.Log("We hit an object");
+
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    
+    public float sidewaysForce = 500f;
+
+    // FixedUpdate for Unity physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);        //on z axis add forward force
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        //if (Input.GetKey("w"))
+        //{
+          //rb.AddForce(ForceMode.VelocityChange, 0, 0, forwardForce * Time.deltaTime);
+        //}
+
+        //if (Input.GetKey("s"))
+        //{
+            //rb.AddForce(ForceMode.VelocityChange, 0, 0, -forwardForce * Time.deltaTime);
+        //}
+
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+
+
+        }
+
+
+    }
+
+
+
+}
